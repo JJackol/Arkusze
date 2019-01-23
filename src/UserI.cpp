ab5ceddaeef161d5c68c2014e51568aa72791ce8
JJackol <jacekczerwonka@gmail.com>
2019-01-23 12:17:29 +0100

    user interface

--- src/UserI.cpp
+++ src/UserI.cpp
@@ -1,9 +1,51 @@
 #include "UserI.h"
 
-UserI::UserI(){
-	
+UserI::UserI(Arkusz* ark){	
+	a=ark;
+	menu="1.Nowy arkusz\n";
+	menu+=	"2.Usun arkusz\n";
+	menu+=	"3.Edytuj arkusz\n";
+	menu+=	"4.Zapisz arkusz";
+	menu+=	"5.Wczytaj arkusz";
 }
 UserI::~UserI(){
+		
+}
+
+void UserI::mainLoop(){
+	int select=0;
+	do{
+		print(*a);
+		printMenu();
+		select=getInt("Wprowadz liczbe  [0 aby wyjsc]");
+		
+		switch(select){
+			case 1:
+				newArkusz(a);
+				break;
+			case 2:
+				break;
+				deleteArkusz();
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+
+			default:
+				break;
+		
+		}
+		
+		
+	}while(select);
 	
 }
 
@@ -25,3 +67,26 @@ void UserI::sayHello(){
 	cout<<helloM<<endl;
 	
 }
+string UserI::prompt(string m){
+	string c="";
+	cout<<m<<endl;
+	cin>>c;
+	return c;
+}
+
+char UserI::getChar(string m){
+	char c=0;
+	cout<<m<<endl;
+	cin>>c;
+	return c;
+}
+int UserI::getInt(string m){
+	int c=0;
+	cout<<m<<endl;
+	cin>>c;
+	return c;
+}
+void UserI::printMenu(){
+		cout<<menu<<endl;
+	}
+
